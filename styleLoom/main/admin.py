ac6bcaa81b0c3e53402d89018ece14dde982785a
Aziz <149665645+mendes-low@users.noreply.github.com>
2025-03-12 17:30:01 +0500

    Final commit

--- styleLoom/main/admin.py
+++ styleLoom/main/admin.py
@@ -2,7 +2,3 @@ from django.contrib import admin
 from .models import Clothes
 
 admin.site.register(Clothes)
-
-
-# class ClothesAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'price', 'image']
--- styleLoom/main/views.py
+++ styleLoom/main/views.py
@@ -1,24 +1,33 @@
-from django.shortcuts import render, get_object_or_404, redirect
-# from .forms import ProductForm
+from django.shortcuts import render, get_object_or_404
 from .models import Clothes
 
 
 def home(request):
-    return render(request, 'home.html')
+    products = Clothes.objects.all()
+    return render(request, 'home.html', {'products': products})
+
 
 def contact(request):
     return render(request, 'contact.html')
+
 
 def products(request):
     gender_filter = request.GET.get('gender', 'all')
 
     if gender_filter == 'all':
-        products = Clothes.objects.all()  
+        products = Clothes.objects.all()
     else:
-        products = Clothes.objects.filter(gender=gender_filter)  
+        products = Clothes.objects.filter(gender=gender_filter)
 
     return render(request, 'products.html', {'products': products, 'gender_filter': gender_filter})
 
+
 def product_detail(request, id):
     product = get_object_or_404(Clothes, id=id)
-    return render(request, 'product_detail.html', {'product': product})
+    related_products = Clothes.objects.filter(gender=product.gender).exclude(
+        id=product.id)[:4] 
+
+    return render(request, 'product_detail.html', {
+        'product': product,
+        'related_products': related_products 
+    })
